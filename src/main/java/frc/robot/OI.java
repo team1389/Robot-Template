8943d6f7ca1c11a6fde19c01787209e37adc7174
Zach Wheeler <92531055+1352351253@users.noreply.github.com>
2025-04-09 13:16:41 -0400

    fixed a silly issue

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -1,6 +1,9 @@
 package frc.robot;
 
+import com.pathplanner.lib.commands.PathPlannerAuto;
+
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.Command;
 
 public class OI {
 
@@ -16,13 +19,15 @@ public class OI {
      * Initialize JoystickButtons and Controllers
      */
     private void initControllers() {
+        //Just bc it says xbox controller, doesn't mean you have to use an xbox controller.
+        //Look at the raw inputs in driverstation and you can figure out what buttons to use. 
         driveController = new XboxController(0);
         manipController = new XboxController(1);
-
     }
 
     private void configureBindings() {
         //PUT YOUR COMMANDS in here! Default commands go first.  
+        
     }
 
     public Command getAutonomousCommand() {
