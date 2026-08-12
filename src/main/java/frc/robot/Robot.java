11a5e1e509df8b40f2d83a6f2f133bdb2a6a0457
Riya Jain <237145384+riy8j@users.noreply.github.com>
2026-08-11 21:18:59 -0400

    adding and applying spotless to be tested

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,61 +1,42 @@
 package frc.robot;
 
-
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
-/**
- * Don't change the name of this class since the VM is set up to run this
- */
+/** Don't change the name of this class since the VM is set up to run this */
 public class Robot extends TimedRobot {
 
-    /**
-     * Initialize all systems here as public & static.
-     * Ex: public static System system = new System();
-     */
+  /**
+   * Initialize all systems here as public & static. Ex: public static System system = new System();
+   */
+  public static OI oi = new OI();
 
+  @Override
+  public void robotInit() {}
 
-    public static OI oi = new OI();
+  /**
+   * This function is called every robot packet, no matter the mode. Use this for items like
+   * diagnostics that you want ran during disabled, autonomous, teleoperated and test.
+   */
+  @Override
+  public void robotPeriodic() {
+    CommandScheduler.getInstance().run();
+  }
 
+  @Override
+  public void autonomousInit() {
+    // Example of setting auto: Scheduler.getInstance().add(YOUR AUTO);
+  }
 
-    @Override
-    public void robotInit() {
-    }
+  /** This function is called periodically during autonomous. */
+  @Override
+  public void autonomousPeriodic() {}
 
-    /**
-     * This function is called every robot packet, no matter the mode. Use
-     * this for items like diagnostics that you want ran during disabled,
-     * autonomous, teleoperated and test.
-     */
-    @Override
-    public void robotPeriodic() {
-        CommandScheduler.getInstance().run();
-    }
+  /** This function is called periodically during operator control. */
+  @Override
+  public void teleopPeriodic() {}
 
-
-    @Override
-    public void autonomousInit() {
-        //Example of setting auto: Scheduler.getInstance().add(YOUR AUTO);
-    }
-
-    /**
-     * This function is called periodically during autonomous.
-     */
-    @Override
-    public void autonomousPeriodic() {
-    }
-
-    /**
-     * This function is called periodically during operator control.
-     */
-    @Override
-    public void teleopPeriodic() {
-    }
-
-    /**
-     * This function is called periodically during test mode.
-     */
-    @Override
-    public void testPeriodic() {
-    }
+  /** This function is called periodically during test mode. */
+  @Override
+  public void testPeriodic() {}
 }
